6254d097bc794cb73ae4ec957b26c74679691331
Vladimir Sobko <vovan.kudo@gmail.com>
2020-12-15 14:31:40 +0300

    add value for mounth range

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,26 @@
-const sliderElement = document.querySelector('#credit-slider');
+const creditRange = document.querySelector('#credit-slider');
 const creditInput = document.getElementById('credit-input');
+let sumOfCredit = 300000;
+const termInput = document.getElementById('term-input-id');
+const termRange = document.getElementById('term-range-id');
 
 
-sliderElement.addEventListener('input', (e) => {
-  document.getElementById('credit-input').value = sliderElement.value + " " + "P";
+
+creditRange.addEventListener('input', (e) => {
+  sumOfCredit = creditRange.value;
+  creditInput.value = creditRange.value + " " + "P";
 });
 
 creditInput.addEventListener('input', (e) => {
-  sliderElement.value = creditInput.value;
-  console.log("creditInput", creditInput.value);
+  creditRange.value = creditInput.value;
 });
 
-
-
-
-// function rangeValFunc(rangeVal){
-//   var rangeWidth = document.getElementById("tooltiptext").textContent = rangeVal+"cm";
-//   document.getElementById("tooltiptext").style.left = "calc("+rangeVal+"% - 50px)";
-// }
+termRange.addEventListener('input', (e) => {
+  if(termRange.value < 12) {
+    termInput.value = `${termRange.value} мес.`;
+  } else if(termRange.value >= 12) {
+    const max = 20 * 12;
+    let countMounth = (max - (max - termRange.value));
+    termInput.value = countMounth;
+  } 
+});
