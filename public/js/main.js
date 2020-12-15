c9d40328acd3b04754c11da25714aea8e5f8695f
Vladimir Sobko <vovan.kudo@gmail.com>
2020-12-15 15:18:14 +0300

    add mounth pay result

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,28 @@
 const creditRange = document.querySelector('#credit-slider');
 const creditInput = document.getElementById('credit-input');
 let sumOfCredit = 300000;
+let term = 1;
 const termInput = document.getElementById('term-input-id');
 const termRange = document.getElementById('term-range-id');
+let rate = 8.8 / 12;
+let mounthPay = 0; 
 
+function getMounthPay() {
+  mounthPay =  sumOfCredit * (rate * Math.pow(1 + rate / 100, term) / 100 / (Math.pow(1 + rate / 100, term) - 1));
+  console.log("mounthPay", Math.round(mounthPay));
+  document.getElementById('mounth-pay-id').textContent = `Ваш ежемесячный платеж * - ${mounthPay.toFixed(0)} ₽`
+}
 
+// S - сумма кредита, руб.
+// T - срок кредита, мес.
+// P = 8.8 / 12 - ставка в месяц
+// S * (P * Math.pow(1 + P / 100, T) / 100 / (Math.pow(1 + P / 100, T) - 1))
+// При изменении полей “Желаемая сумма кредита” и “Срок кредита” необходимо рассчитывать ежемесячный платеж и выводить на форме
 
 creditRange.addEventListener('input', (e) => {
   sumOfCredit = creditRange.value;
   creditInput.value = creditRange.value + " " + "P";
+  getMounthPay();
 });
 
 creditInput.addEventListener('input', (e) => {
@@ -17,10 +31,12 @@ creditInput.addEventListener('input', (e) => {
 
 termRange.addEventListener('input', (e) => {
   if(termRange.value < 12) {
+    term = termRange.value;
     termInput.value = `${termRange.value} мес.`;
   } else if(termRange.value >= 12) {
     const max = 20 * 12;
-    let countMounth = (max - (max - termRange.value));
-    termInput.value = countMounth;
+    term = (max - (max - termRange.value));
+    termInput.value = term;
+    getMounthPay();
   } 
 });
