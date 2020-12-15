7768382e4832d94638f8fd96ebffac09d31b07e5
Vladimir Sobko <vovan.kudo@gmail.com>
2020-12-15 17:20:12 +0300

    add validation

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,17 @@
 const creditRange = document.querySelector('#credit-slider');
 const creditInput = document.getElementById('credit-input');
 let sumOfCredit = 300000;
-let term = 1;
 const termInput = document.getElementById('term-input-id');
 const termRange = document.getElementById('term-range-id');
-let rate = 8.8 / 12;
-let mounthPay = 0; 
+const monthPaymentSpan = document.getElementById('month-payment');
 
-function getMounthPay() {
-  mounthPay =  sumOfCredit * (rate * Math.pow(1 + rate / 100, term) / 100 / (Math.pow(1 + rate / 100, term) - 1));
-  console.log("mounthPay", Math.round(mounthPay));
-  document.getElementById('mounth-pay-id').textContent = `Ваш ежемесячный платеж * - ${mounthPay.toFixed(0)} ₽`
-}
-
-// S - сумма кредита, руб.
-// T - срок кредита, мес.
-// P = 8.8 / 12 - ставка в месяц
-// S * (P * Math.pow(1 + P / 100, T) / 100 / (Math.pow(1 + P / 100, T) - 1))
-// При изменении полей “Желаемая сумма кредита” и “Срок кредита” необходимо рассчитывать ежемесячный платеж и выводить на форме
 
 creditRange.addEventListener('input', (e) => {
   sumOfCredit = creditRange.value;
   creditInput.value = creditRange.value + " " + "P";
-  getMounthPay();
+  creditRange.step = customStep(creditRange.value);
+
+  monthPayment();
 });
 
 creditInput.addEventListener('input', (e) => {
@@ -31,12 +20,33 @@ creditInput.addEventListener('input', (e) => {
 
 termRange.addEventListener('input', (e) => {
   if(termRange.value < 12) {
-    term = termRange.value;
     termInput.value = `${termRange.value} мес.`;
-  } else if(termRange.value >= 12) {
-    const max = 20 * 12;
-    term = (max - (max - termRange.value));
-    termInput.value = term;
-    getMounthPay();
+  } else {
+    var years = (termRange.value / 12).toFixed();
+    var months = termRange.value % 12;
+    termInput.value = `${years} лет и ${months} мес.`;
   } 
+
+  monthPayment();
 });
+
+
+function customStep(value) {
+  var step = 50000;
+  value = parseFloat(value);
+  if (value > 3000000) { step = 500000}
+  else if (value > 1000000) { step = 100000 }
+
+  return step;
+}
+
+
+function monthPayment() {
+ var sum = creditRange.value; // S
+ var term = termRange.value; // T
+ var percent = 8.8/12; // P
+
+ var monthPayment = sum * (percent * Math.pow(1 + percent/100, term) / 100 / (Math.pow(1 + percent/100, term)-1));
+
+  monthPaymentSpan.textContent = monthPayment.toFixed(2);
+}
